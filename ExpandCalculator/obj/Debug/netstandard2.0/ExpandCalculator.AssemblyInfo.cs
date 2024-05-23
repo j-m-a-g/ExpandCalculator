37e76f4fdc5dc0eec67a0c61a286d5a799729287
j-m-a-g <jmaghamez@gmail.com>
2024-05-23 15:42:35 -0400

    Last Changes

--- ExpandCalculator/obj/Debug/netstandard2.0/ExpandCalculator.AssemblyInfo.cs
+++ ExpandCalculator/obj/Debug/netstandard2.0/ExpandCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpandCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84ccecad0ffdbbcc0745261be208eef000eba753")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpandCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpandCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
